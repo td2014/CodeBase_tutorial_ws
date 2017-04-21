9f95f9afab2b0cf6bbdb9b770216bd53aeca332c
td2014 <anthonyd2004@gmail.com>
2017-04-21 10:39:46 -0700

    Update.

--- src/dd_process_code/scripts/listener_image_pub.py
+++ src/dd_process_code/scripts/listener_image_pub.py
@@ -85,7 +85,7 @@ def callback(data):
         yOffset=50 #meters
         ringRangeAvg=np.zeros([1,32])  # Store range^2 for each ring
         ringRangeInit=np.zeros([1,32]) # Keeps track of initialized ring range estimates
-        rangeThresh = 0.8  # range^2 less than this factor will be considered detections
+        rangeThresh = 0.81  # range^2 less than this factor will be considered detections
         detx = 0.0  # average x position of detection
         dety = 0.0  # average y position of detection
         pointCount = 0 # number of points in detection 
@@ -108,8 +108,8 @@ def callback(data):
 
             xIdx = int((x[0]+xOffset)*xScale)
             yIdx = int((y[0]+yOffset)*yScale)
-###            if xIdx < xSize and yIdx < ySize and x[0]>0 and ringNum[0]>11 and ringNum[0]<20:
-            if xIdx < xSize and yIdx < ySize and x[0]>0 and ringNum[0]==17:
+            theta = np.arctan2(y[0],x[0])*180.0/np.pi  # angle of return
+            if xIdx < xSize and yIdx < ySize and x[0]>1.0 and ringNum[0]>11 and ringNum[0]<20 and abs(theta)<45.0:
 ###                imageGrid[xIdx,yIdx] = 1.0
                 newRange = x[0]*x[0] + y[0]*y[0] 
                 if ringRangeInit[0,ringNum[0]] < 3:  # keep adding to average range calc window
@@ -144,7 +144,7 @@ def callback(data):
         if pointCount > 0:
            x = detx / (1.0*pointCount)
            y = dety / (1.0*pointCount) 
-           z = 1.0 # nominal
+           z = -0.6 # nominal
            publishEstm =True
         else:  # don't publish
            publishEstm = False
@@ -164,11 +164,11 @@ def callback(data):
 #
 # Display bird-eye image
 #                
-###        print('Timestamp (sec) = ', data.header.stamp.secs)
-###        print('Published x, y, z = ', x, y, z)
-###        cv2.imshow('image',imageGrid)
-###        cv2.waitKey(0)
-###        cv2.destroyAllWindows()
+        print('Timestamp (sec) = ', data.header.stamp.secs)
+        print('Published x, y, z = ', x, y, z)
+        cv2.imshow('image',imageGrid)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 #
 # End of callback
